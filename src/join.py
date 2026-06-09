a863d5f9a920104a38571611164ca380bd58e368
Reyhashem <115505819+Reyhashem@users.noreply.github.com>
2026-06-09 19:44:55 +0330

    message and join

---
+++ src/join.py
@@ -0,0 +1,107 @@
+import asyncio
+from datetime import datetime
+from prisma.enums import Role
+
+from prisma import Prisma
+
+async def list_groupchats(db: Prisma):
+    user_id = int(input("user ID: "))
+    join = await db.GroupJoined.find_many(
+       where={"userId":user_id},
+       orderBy={"joinedAt": "desc"}
+       )
+    if len(join) > 0: 
+        print(f"user {user_id} ")
+        for j in join:
+         print(f"joined group {j.groupId} at {j.joinedAt}")
+    else:
+      print(f"user [{user_id}] have not join any group yet.")
+
+async def change_role(db: Prisma):
+    group_id = int(input("Group ID: "))
+    user_id = int(input("User ID: "))
+    role = input("Role (USER, ADMIN or OWNER): ")
+    if role not in ["USER", "ADMIN", "OWNER"]:
+        print("Invalid role.")
+        return
+    try:
+        await db.GroupJoined.update(
+            where={"groupId_userId": {
+                "groupId": group_id,"userId": user_id}},
+            data={"role":role}
+            )
+        print(f"successfully, changed user {user_id} role into {role} in {group_id}")
+    except Exception as e:
+        print(f"Failed to change user {user_id} role in {group_id}")
+
+async def OwnersAdmins (db: Prisma):
+    group_id = int(input("Group ID: "))
+    try:
+     groups = await db.GroupJoined.find_many(
+        where={
+            "groupId": group_id ,
+            "OR": [
+                { "role": Role.ADMIN  },
+                { "role": Role.OWNER  },] }
+     )
+     return groups
+    except Exception as e:
+        print(f"Failed to find the group.")
+
+async def Users_Role (db: Prisma):
+    group_id = int(input("Group ID: "))
+    user_id = int(input("User ID: "))
+    try:
+     user = await db.GroupJoined.find_unique(
+        where={
+             "groupId_userId": {
+                    "groupId": group_id,
+                    "userId": user_id }}
+     )
+     if user is None:
+            print("User is not in this group.")
+            return None 
+     return user.role
+    except Exception as e:
+        print(f"Failed to find.")
+
+async def existence_user (db: Prisma):
+    group_id = int(input("Group ID: "))
+    user_id = int(input("User ID: "))
+    user = await db.GroupJoined.find_unique(
+        where={
+            "groupId_userId": {
+                "groupId": group_id,
+                "userId": user_id } }
+     )
+    if user is None:
+        print(f"User {user_id} is NOT in group {group_id}")
+    else:
+        print(f"there is user {user_id} in {group_id} ")
+
+async def group_menu(db: Prisma):
+
+    while True:
+        print("[GROUPJOINED] Choose a query: list_groupchats, change_role, owners/admins, users_role, existence_user or quit")
+        cmd = input("\n> ").strip().lower()
+
+        if cmd == "quit":
+            return
+        elif cmd == "list_groupchats":
+            await list_groupchats(db)
+        elif cmd == "change_role":
+            await change_role(db)
+        elif cmd == "owners/admins":
+            groups = await OwnersAdmins(db)
+            if not groups:
+              print("No admins/owners found")
+            else:
+                for g in groups:
+                    print(g)
+        elif cmd == "users_role":
+            role = await Users_Role(db)
+            print(role)
+        elif cmd == "existence_user":
+            await existence_user(db)
+        else:
+            print("Unknown command.")
---
+++ src/message.py
@@ -0,0 +1,173 @@
+import asyncio
+from datetime import datetime
+
+from prisma import Prisma
+
+
+async def list_messages(db: Prisma):
+    group_id = int(input("group ID: "))
+    messages = await db.message.find_many(
+       where={"groupId":group_id})
+    if len(messages) > 0: 
+        for m in messages:
+         print(f"[{m.id}] {m.content}")
+    else:
+      print("no message had been send yet.")
+
+
+async def delete(db: Prisma):
+    message_id = int(input("Message ID: "))
+    user_id = int(input("User ID: "))
+    message = await db.message.find_unique(
+        where={"id": message_id}
+    )
+    if (message is None):
+        print(f"the message does not exist.")
+    else:
+      try:
+        if(message.senderId == user_id):
+         await db.message.delete(
+             where={"id": message_id}
+        )
+         print(f"message successfully deleted")
+        else:
+           print(f"Only the sender can delete the message sent.")
+      except Exception as e:
+        print(f"Failed to remove message.")   
+
+
+async def reply(db: Prisma):
+    message_id = int(input("message ID: "))
+    user_id = int(input("User ID: "))
+    content = input("message : ")
+    try:
+        message = await db.message.find_unique(
+        where={"id": message_id})
+        if message is None:
+            print("Message not found.")
+            return
+        if message.groupId > 0 or message.reciverId >0:
+         if message.groupId > 0:
+           rep = await db.message.create(
+            data={"content": content, "senderId": user_id , "groupId": message.groupId,"replyId":message.id ,"timestamp": datetime.now()})
+           print(f"user {user_id} replied to message {message_id} with {rep.content}")
+         else:
+           rep = await db.message.create(
+            data={"content": content, "senderId": user_id , "reciverId": message.senderId,"replyId":message.id ,"timestamp": datetime.now()})
+           print(f"user {user_id} replied to message {message_id} with {rep.content}")
+        else:
+            print(f"Failed to reply the message")
+    except Exception as e:
+        print(f"Failed to reply the message")
+
+
+async def send(db: Prisma):
+    content = input("message : ")
+    sender_id = int(input("sender ID : "))
+    group_id = int(input("Group ID or 0: "))
+    user_id = int(input("reciver ID or 0: "))
+    try:
+        sender = await db.user.find_unique(
+        where={"id": sender_id})
+        if(group_id !=0 and user_id ==0):
+         group = await db.group.find_unique(
+         where={"id": group_id})
+         message = await db.message.create(data={"content": content, "senderId": sender_id , "groupId": group_id ,"timestamp": datetime.now()})
+         print(f"user {sender.name} send message in {group.name}")
+        elif(group_id ==0 and user_id !=0):
+         reciver = await db.user.find_unique(
+         where={"id": user_id})
+         message = await db.message.create(data={"content": content, "senderId": sender_id , "receiverId": user_id ,"timestamp": datetime.now()})
+         print(f"user  {sender.name} send message to {reciver.name}")
+        else:
+         print(f"Failed to send the message")
+    except Exception as e:
+        print(f"Failed to send the message")
+
+
+async def recent_chat(db: Prisma):
+    sender_id = int(input("sender ID : "))
+    message = await db.message.find_first(
+     where={"senderId": sender_id},
+     orderBy={"timestamp": "desc"}
+    )
+    if message:
+     print(message.content)
+    else:
+     print(f"recently no message has been sent")
+
+
+async def get_replies(db: Prisma):
+   message_id = int(input("message ID: "))
+   try:
+    message = await db.message.find_first(
+        where={"id": message_id}
+    )
+    replies = await db.message.find_many(
+        where={"replyId": message_id}
+    )
+    print(message.content)
+    for r in replies:
+     print(r.content)
+   except:
+    print(f"Failed to find the message")
+
+
+async def search_by_sender(db: Prisma):
+    group_id = int(input("group ID: "))
+    user_id = int(input("user ID: "))
+    messages = await db.message.find_many(
+        where={"groupId": group_id , "senderId": user_id}
+    )
+    if len(messages) == 0:
+        print(f"user did not send message in group.")
+    else:
+        for m in messages:
+            print(m.content)
+
+async def edit_message(db: Prisma):
+    text = input("new message : ")
+    message_id = int(input("Message ID: "))
+    user_id = int(input("User ID: "))
+    message = await db.message.find_unique(
+        where={"id": message_id}
+    )
+    if (message is None):
+        print(f"the message does not exist.")
+    else:
+        if(message.senderId == user_id):
+         await db.message.update(
+            where={"id": message_id},
+            data={"content":text}
+          )
+         print(f"message successfully edited")
+        else:
+           print(f"Only the sender can edit the message sent.") 
+    
+
+async def message_menu(db: Prisma):
+
+    while True:
+        print("[message] Choose a query: list_messages, edit_message, delete, reply, search_by_sender, send, recent_chat, get_replies or quit")
+        cmd = input("\n> ").strip().lower()
+
+        if cmd == "quit":
+            return
+        elif cmd == "list_messages":
+            await list_messages(db)
+        elif cmd == "delete":
+            await delete(db)
+        elif cmd == "reply":
+            await reply(db)
+        elif cmd == "send":
+            await send(db)
+        elif cmd == "recent_chat":
+            await recent_chat(db)
+        elif cmd == "get_replies":
+            await get_replies(db)
+        elif cmd == "search_by_sender":
+            await search_by_sender(db)
+        elif cmd == "edit_message":
+            await edit_message(db)
+        else:
+            print("Unknown command.")
