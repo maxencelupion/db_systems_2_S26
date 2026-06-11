9a22774f18460353ae8c22d3bdd33967b33765bc
Reyhashem <115505819+Reyhashem@users.noreply.github.com>
2026-06-12 02:00:13 +0330

    add trigger and procedure

--- prisma/migrations/20260602000000_add_update_group_last_message_trigger/migration.sql
+++ prisma/migrations/20260602000000_add_update_group_last_message_trigger/migration.sql
@@ -5,3 +5,25 @@ FOR EACH ROW
 BEGIN
   UPDATE `Group` SET lastMessageAt = NOW() WHERE id = NEW.groupId;
 END;
+
+-- CreateTrigger: user can not send empty message 
+CREATE TRIGGER empty_message
+BEFORE INSERT ON Message
+FOR EACH ROW
+BEGIN
+  IF (NEW.content IS NULL OR NEW.content = '') THEN
+     RAISE EXCEPTION 'can not send empty message';
+  END IF;  
+END;
+
+
+CREATE PROCEDURE private_chat(
+IN first_user INT
+IN second_user INT)
+BEGIN
+  SELECT *
+  FROM Message
+  WHERE (senderId = first_user AND receiverId = second_user)
+  OR
+  (senderId = second_user AND receiverId = first_user);
+END;
